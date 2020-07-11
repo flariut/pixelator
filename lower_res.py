aab90d406ceffa27de70b95bf96ec8067722c998
flariut <flariut@gmail.com>
2020-07-11 20:35:11 -0300

    def - subido a youtube

--- lower_res.py
+++ lower_res.py
@@ -3,6 +3,7 @@ import glob
 
 def main():
     SIZE = (1920, 1080)
+    ASP = SIZE[0] / SIZE[1]
 
     img_array = []
 
@@ -13,19 +14,27 @@ def main():
 
         if src.shape[:2][::-1] == SIZE:
             for tier in range(1, 4):
-                for res in range(10, 0 if tier == 3 else 1, -1):
-                    width = int(SIZE[0] * res / 10 ** tier)
-                    height = int(SIZE[1] * res / 10 ** tier)
+                for step in range(9, 0, -1):
+
+                    height = round(SIZE[1] * step / 10 ** tier)
+                    width = round(height * ASP)
                     dim = (width, height)
                     
                     # Reduce the resolution
                     resized = cv2.resize(src, dim,
                                          interpolation = cv2.INTER_AREA)
                     # Back to 1080 to create the video frame
-                    resized = cv2.resize(resized, SIZE,
+                    frame = cv2.resize(resized, SIZE,
                                          interpolation = cv2.INTER_AREA)
-                    img_array.append(resized)
-
+                    
+                    img_array.append(frame)
+            
+            # Last single "pixel" frame
+            resized = cv2.resize(src, (1,1),
+                                 interpolation = cv2.INTER_AREA)
+            frame = cv2.resize(resized, SIZE,
+                               interpolation = cv2.INTER_AREA)
+            img_array.append(frame)
 
     out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 2,
                           SIZE)
