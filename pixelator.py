b33e1888e121c7a3fb41dd7a60def7a8539231a4
flariut <flariut@gmail.com>
2020-07-12 03:44:48 -0300

    new version0

--- pixelator.py
+++ pixelator.py
@@ -1,47 +1,44 @@
 import cv2
 import glob
+from math import log
 
 def main():
-    SIZE = (1920, 1080)
-    ASP = SIZE[0] / SIZE[1]
+    size = (1920, 1080)
+    fps = 30
+    duration = 5
+    pixelator(size, fps, duration)
 
-    img_array = []
+def pixelator(size, fps, duration):
+
+    asp = size[0] / size[1]
+    steps = duration * fps
+    decr = (1/size[1]) ** (1/steps)
+    
+    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps,
+                          size)
 
     for image_path in sorted(glob.glob('sources/*.jpg')):
 
         src = cv2.imread(image_path)
-        img_array.append(src)
+        out.write(src)
+        _res = size
+        
+        if src.shape[:2][::-1] == size:
+            for step in range(0, steps, 1):
+                
+                height = int(size[1] * decr ** step)
+                width = round(height * asp)
+                _res = (width, height)
 
-        if src.shape[:2][::-1] == SIZE:
-            for tier in range(1, 4):
-                for step in range(9, 0, -1):
-
-                    height = round(SIZE[1] * step / 10 ** tier)
-                    width = round(height * ASP)
-                    dim = (width, height)
-                    
-                    # Reduce the resolution
-                    resized = cv2.resize(src, dim,
-                                         interpolation = cv2.INTER_AREA)
-                    # Back to 1080 to create the video frame
-                    frame = cv2.resize(resized, SIZE,
-                                         interpolation = cv2.INTER_AREA)
-                    
-                    img_array.append(frame)
-            
-            # Last single "pixel" frame
-            resized = cv2.resize(src, (1,1),
-                                 interpolation = cv2.INTER_AREA)
-            frame = cv2.resize(resized, SIZE,
-                               interpolation = cv2.INTER_AREA)
-            img_array.append(frame)
-
-    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 2,
-                          SIZE)
-
-    for img in img_array:
-        out.write(img)
-
+                # Reduce the resolution
+                resized = cv2.resize(src, _res,
+                                        interpolation = cv2.INTER_AREA)
+                # Back to 1080 to create the video frame
+                frame = cv2.resize(resized, size,
+                                        interpolation = cv2.INTER_AREA)
+                
+                out.write(frame)
+                
     out.release()
 
 if __name__ == '__main__':
