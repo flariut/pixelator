589853bbc5ba3296e0804ac89e290a68a4d40b95
flariut <flariut@gmail.com>
2020-07-12 13:19:10 -0300

    added prints

--- pixelator.py
+++ pixelator.py
@@ -5,16 +5,18 @@ from math import log
 def main():
     size = (1920, 1080)
     fps = 30
-    duration = 5
-    pixelator(size, fps, duration)
+    duration = 15
+    output_path = 'project.avi'
+    pixelator(size, fps, duration, output_path)
 
-def pixelator(size, fps, duration):
+def pixelator(size, fps, duration, output_path):
 
     asp = size[0] / size[1]
     steps = duration * fps
     decr = (1/size[1]) ** (1/steps)
-    
-    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps,
+    print('##', size, fps, duration, round(asp,2), steps, round(decr,2))
+    print('## Starting...')
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'DIVX'), fps,
                           size)
 
     for image_path in sorted(glob.glob('sources/*.jpg')):
@@ -22,6 +24,8 @@ def pixelator(size, fps, duration):
         src = cv2.imread(image_path)
         out.write(src)
         _res = size
+        
+        print(f'### Pixelating {image_path}...')
         
         if src.shape[:2][::-1] == size:
             for step in range(0, steps, 1):
@@ -32,14 +36,15 @@ def pixelator(size, fps, duration):
 
                 # Reduce the resolution
                 resized = cv2.resize(src, _res,
-                                        interpolation = cv2.INTER_AREA)
+                                     interpolation = cv2.INTER_AREA)
                 # Back to 1080 to create the video frame
                 frame = cv2.resize(resized, size,
-                                        interpolation = cv2.INTER_AREA)
+                                   interpolation = cv2.INTER_AREA)
                 
                 out.write(frame)
                 
     out.release()
 
 if __name__ == '__main__':
+    print('# Pixelator')
     main()
